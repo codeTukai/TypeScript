46438ccdcafdb4027a42faf507976cbc87b9d6f4
codeTukai <tukaighosh753@gmail.com>
2026-05-01 23:28:11 +0530

    first commit

--- 01_TypeScript/src/App.tsx
+++ 01_TypeScript/src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Card from './components/Card'
 
 function App() {
  
 
   return (
   <>
-  <h1>Vite and React</h1>
+  <div>
+    <h1>Vite and React</h1>
+    <Card name="Headphone" price="5000"/>
+  </div>
   </>
   )
 }
---
+++ 01_TypeScript/src/components/Card.tsx
@@ -0,0 +1,14 @@
+
+
+export function Card({name, price,isSpecial = false}) {
+  return (
+    <article>
+        <h2>
+            {name} {isSpecial && <span>**</span>}
+        </h2>
+        <p>{price}</p>
+    </article>
+  )
+}
+
+export default Card
