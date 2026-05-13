1863d99f95af9f88be2e56e88b5c680607b4f430
codeTukai <tukaighosh753@gmail.com>
2026-05-13 12:13:54 +0530

    set up for ts

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet() {
+}
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function greet(name: string):string{
+    return `Hello ${name} Hii, i am a developer`
+}
+
+const username: string = "Jack"
+
+console.log(greet(username));
