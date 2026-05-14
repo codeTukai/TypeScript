6929ca15e084890895160cf3600aec926cb0979b
codeTukai <tukaighosh753@gmail.com>
2026-05-14 20:47:57 +0530

    feat: add type assertion and type guard examples in moreTypes.ts

---
+++ src/moreTypes.ts
@@ -0,0 +1,67 @@
+//type assertion
+let response:any = "20";
+
+let numberLength : number = (response as string).length //forcefully convert into another data types
+
+type Book = {
+    name : string;
+};
+
+let bookString = '{"name": "who moved my cheese"}'
+let bookObject = JSON.parse(bookString) as Book
+
+console.log(bookObject);
+
+const inputElement = document.getElementById("username") as HTMLInputElement;
+let value : any 
+
+value = "chai"
+value = [1,2,3]
+value = 2.5
+
+value.toUpperCase()
+
+let newValue : unknown
+
+newValue = "chai"
+newValue = [1,2,3]
+newValue = 2.5
+
+if (typeof newValue === "string") {
+    newValue.toUpperCase()
+}
+
+
+try {
+//type guard    
+} catch (error) {
+    if (error instanceof Error) {
+        console.log(error.message);
+        
+    }
+    console.log("Error", error);
+    
+}
+
+const data: unknown = "chai aur code"
+const strData: string = data as string
+
+console.log(data);
+
+type Role = "admin" | "user" | "superAdmin"
+
+function roleBased(item:Role): void{
+    if (item ==="admin") {
+        console.log("Role provided to admin");
+        return
+    }
+    if(item === "user"){
+        console.log("Redirected based on User");
+        return
+    }
+    item;
+}
+
+function neverReturn(): never{
+    while(true){}
+}
