3f172e35d7dea4f2279f63daec42e7b07679ef29
codeTukai <tukaighosh753@gmail.com>
2026-05-18 10:02:16 +0530

    practice ts

--- src/fetchdata.ts
+++ src/fetchdata.ts
@@ -1,3 +1,29 @@
+import axios from "axios";
+import type { AxiosResponse } from "axios"
+
+//define data types is the main factor 
+interface Todo {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}
+
 const fetchData = async()=>{
-    
+    try {
+        const response:AxiosResponse<Todo> = await axios.get("https://jsonplaceholder.typecode.com/todos/1")
+        console.log("Todo", response.data);
+        
+    } catch (error:any) {
+        if(axios.isAxiosError(error)){
+         console.log("Axios Error",error.message);
+
+         if (error.response) {
+            console.log(error.response.status);
+            
+        }
+        }
+        
+        
+    }
 }
